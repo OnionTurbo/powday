87283c5592b73883b8c892e036a0dd12e1a3703c
OnionTurbo <ericpeterson175@u.boisestate.edu>
2020-10-25 22:31:58 -0600

    Missing some old files

---
+++ flask-back/static/react/precache-manifest.55f67db598ad802f65e706f56b96ab37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c66c44d535f87291e6f58e9c7645ca3",
+    "url": "/static/react/..\\..\\templates\\index.html"
+  },
+  {
+    "revision": "217add32cdb485e8e08f",
+    "url": "/static/react/css/2.6e6f38cb.chunk.css"
+  },
+  {
+    "revision": "11bfee4df3438107d3e2",
+    "url": "/static/react/css/main.daf07dbe.chunk.css"
+  },
+  {
+    "revision": "217add32cdb485e8e08f",
+    "url": "/static/react/js/2.8c88faf4.chunk.js"
+  },
+  {
+    "revision": "3eab71aebc406384da020eba571591e6",
+    "url": "/static/react/js/2.8c88faf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11bfee4df3438107d3e2",
+    "url": "/static/react/js/main.ade7ec2b.chunk.js"
+  },
+  {
+    "revision": "6fe04c63e34fcc91b4af",
+    "url": "/static/react/js/runtime-main.3c2f4aba.js"
+  }
+]);
