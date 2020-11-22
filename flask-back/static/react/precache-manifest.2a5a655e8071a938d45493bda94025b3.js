c7c27fc1297a7e2a828198447562d96847210ee0
OnionTurbo <ericpeterson175@u.boisestate.edu>
2020-11-21 19:33:52 -0700

    Adding Nikita's changes

---
+++ flask-back/static/react/precache-manifest.2a5a655e8071a938d45493bda94025b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f459f8fe4234986898f8c8daec326b0",
+    "url": "/static/react/..\\..\\templates\\index.html"
+  },
+  {
+    "revision": "14686124e1253b41d88a",
+    "url": "/static/react/css/2.6e6f38cb.chunk.css"
+  },
+  {
+    "revision": "188d5e08ddd77b8bb966",
+    "url": "/static/react/css/main.daf07dbe.chunk.css"
+  },
+  {
+    "revision": "14686124e1253b41d88a",
+    "url": "/static/react/js/2.15ec8607.chunk.js"
+  },
+  {
+    "revision": "3eab71aebc406384da020eba571591e6",
+    "url": "/static/react/js/2.15ec8607.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "188d5e08ddd77b8bb966",
+    "url": "/static/react/js/main.ec2e1dec.chunk.js"
+  },
+  {
+    "revision": "6fe04c63e34fcc91b4af",
+    "url": "/static/react/js/runtime-main.3c2f4aba.js"
+  }
+]);
